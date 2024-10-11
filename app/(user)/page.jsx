4ecfdc5a513dce1f8784e10d80365807fde6b24c
Furkan <furkancosar2005@gmail.com>
2024-10-11 18:23:13 +0300

    linkler çekildi ekrana bastırıldı

--- app/(user)/page.jsx
+++ app/(user)/page.jsx
@@ -1,15 +1,29 @@
+import { createClient } from "@/utils/supabase/server";
 import postLongUrl from "./actions";
+import Link from "next/link";
 
 export default async function Home() {
+  const supabase = createClient();
+
+  let { data: urls, error } = await supabase
+  .from('urls')
+  .select('*');
+
   return (
     <div className="container">
         <h1>link kısaltma sayfası</h1>
 
         <form>
             <input type="text" id="long_url" name="long_url" placeholder="kısaltmak istediğin URL"/>
-
             <button formAction={postLongUrl}>Linki Kısalt</button>
         </form>
+
+        {urls.map(x => 
+          <div className="deneme">
+
+            <Link href={`/`}>{x.short_url}</Link>
+          </div>
+        )}
     </div>
   );
 }
