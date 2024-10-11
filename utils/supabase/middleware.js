6e9e5b51e47f28e98e215be7df6c8a43d3a46b4b
Furkan <furkancosar2005@gmail.com>
2024-10-11 15:39:20 +0300

    layout oluşturuldu

--- utils/supabase/middleware.js
+++ utils/supabase/middleware.js
@@ -38,7 +38,7 @@ export async function updateSession(request) {
   ) {
     const url = request.nextUrl.clone()
     url.pathname = '/login'
-    return NextResponse.redirect(url)
+    return NextResponse.next()
   }
 
   return supabaseResponse
